e6148f04da79ed6806d69752db97c9ef2e2bae34
Roice32 <alexandru.butnaru32@yahoo.com>
2024-07-22 13:52:02 +0300

    Tested jmp

---
+++ tests/inputs/jmp.asm
@@ -0,0 +1,21 @@
+.boot
+    jmp main
+
+.code
+main:
+first:
+    jmp second
+end:
+    end_sim
+
+second:
+    mov r0, 3
+    jmp third
+
+third:
+    cmp 2, 3
+    jl end
+
+.data
+someVar:
+dw 0x0001
