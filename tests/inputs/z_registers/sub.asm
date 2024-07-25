dfd6a5be893618a0e34121a2d6b2487d04de4a61
Roice32 <alexandru.butnaru32@yahoo.com>
2024-07-25 12:41:06 +0300

    Tested sub for Z-Regs

---
+++ tests/inputs/z_registers/sub.asm
@@ -0,0 +1,24 @@
+.boot
+    jmp main
+
+.code
+main:
+    mov [0x2000], 10
+    mov [0x2002], 20
+    mov [0x2004], 30
+    mov [0x2006], 40
+    mov z0, [0x2000]
+
+    mov [0x2000], 1
+    mov [0x2002], 2
+    mov [0x2004], 3
+    mov [0x2006], 4
+    mov z1, [0x2000]
+
+    sub [0x3000], z0
+    sub z0, z1
+    mov r1, 0x3000
+    sub z3, [r1]
+    end_sim
+
+.data
